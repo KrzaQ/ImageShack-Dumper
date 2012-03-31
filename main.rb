74d1ba27578cb5bf5155a7f01bf494fa136f5c1b
KrzaQ <git@debil.eu>
2012-03-31 21:23:23 +0200

    Updated readme, will ask for password and username if not supplied

--- main.rb
+++ main.rb
@@ -14,6 +14,16 @@ ARGV.each do |a|
 	options[sp[0]] = sp[1]
 end
 
+if not options.include?('u'):
+	print 'please type your username: '
+	options['u'] = STDIN.gets.gsub("\n",'')
+end
+
+if not options.include?('p'):
+	print 'please type your password: '
+	options['p'] = STDIN.gets.gsub("\n",'')
+end
+
 if not options.include?('p') or not options.include?('u'):
 	puts "incorrect syntax, options p and u must be used"
 	exit()
